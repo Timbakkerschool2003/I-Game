386cf242c10518f4ce2099b556af40a92e774798
AshrafBasnoe-Github <s1185527@student.windesheim.nl>
2024-05-29 13:04:43 +0200

    bugus fixed

--- IGame/resources/views/home.blade.php
+++ IGame/resources/views/home.blade.php
@@ -59,6 +59,7 @@
     </style>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
     <title>Room Form</title>
     <link rel="stylesheet" href="style.css">
 </head>
--- IGame/tailwind.config.js
+++ IGame/tailwind.config.js
@@ -1,15 +1,12 @@
-module.exports = {
-    purge: [
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
-    ],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {},
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
