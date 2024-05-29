076786c044956b0004939b13722048cd298207f9
AshrafBasnoe-Github <s1185527@student.windesheim.nl>
2024-05-29 16:18:38 +0200

    Getallen vloeiend laten bewegen
    Getallen vloeiend laten bewegen

--- iGame/resources/views/igame.blade.php
+++ iGame/resources/views/igame.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Igame</title>
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="../../css/app.css" rel="stylesheet">
     <script>
         function checkGameOver(week) {
             if (week >= 24) {
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@ const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 
 mix.js('resources/js/app.js', 'public/js')
-   .postCss('resources/css/app.css', 'public/css', [
+   .sass('resources/sass/app.scss', 'public/css', [
       tailwindcss('./tailwind.config.js'),
    ]);
 
